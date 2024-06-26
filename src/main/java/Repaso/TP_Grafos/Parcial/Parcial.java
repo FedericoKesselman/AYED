b71728c718643e79bc114b35276e82c3c71510d2
Federico Kesselman <kesselmanfederico@gmail.com>
2024-06-26 10:07:08 -0300

    .

--- src/main/java/Repaso/TP_Grafos/Parcial/Parcial.java
+++ src/main/java/Repaso/TP_Grafos/Parcial/Parcial.java
@@ -16,37 +16,52 @@ public class Parcial {
 				}
 			}
 			
-			if (origen != null) 
-				ok = resolverHelper (sitios, origen, new boolean[sitios.getSize()], new LinkedList<lugar>(), tiempo-origen.getData().getMinutos());				
+			if (origen != null) {
+				ok = resolverHelper (sitios, origen, new boolean[sitios.getSize()], new LinkedList<>(), tiempo-origen.getData().getMinutos());
+			}
 		}
 		
 		if (ok)
 			return "Alcanzable";
-		else 
+		else
 			return "No Alcanzable";
 	}
 	
-	private boolean resolverHelper (Graph<lugar> sitios, Vertex<lugar> vertex, boolean[] visitados, List<lugar> caminoAct, int tiempo) {
+	private boolean resolverHelper (Graph<lugar> sitios, Vertex<lugar> vertex, boolean[] visitados, List<lugar> camino, int tiempo) {
 		visitados[vertex.getPosition()] = true;
-		caminoAct.add(vertex.getData());
-		boolean ok = false;
+		camino.add(vertex.getData());
+		boolean paro = false;
 		
 		if (tiempo - vertex.getData().getMinutos() >= 0) {
-			
-			if (caminoAct.size() == sitios.getSize())
+			tiempo -= vertex.getData().getMinutos();
+		
+			if (camino.size() == sitios.getSize())
 				return true;
 			else {
 				for (Edge<lugar> edge: sitios.getEdges(vertex)) {
-					int tiempoNecesario = edge.getWeight();
-					if (!visitados[edge.getTarget().getPosition()] && tiempo-vertex.getData().getMinutos()-tiempoNecesario >= 0)
-						ok = resolverHelper (sitios, edge.getTarget(), visitados, caminoAct, tiempo-vertex.getData().getMinutos()-tiempoNecesario);
+					int tiempoArista = edge.getWeight();
+					if (!visitados[edge.getTarget().getPosition()] && !paro && tiempo-tiempoArista >= 0)
+						paro = resolverHelper (sitios, edge.getTarget(), visitados, camino, tiempo-tiempoArista);
 				}
 			}
 		}
-		visitados[vertex.getPosition()] = false;
-		caminoAct.remove(vertex.getData());
-		return ok;
+		
+		if (!paro) {
+			visitados[vertex.getPosition()] = false;
+			camino.remove(camino.size()-1);
+		}
+		return paro;
 	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
